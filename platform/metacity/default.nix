5eb75b1b12e36e78199f6f98ab82adb1d25ad64d
Katou Megumi <electi@yzu.moe>
2026-03-12 17:50:35 +0000

    sorrgy metacity

--- platform/metacity/default.nix
+++ platform/metacity/default.nix
@@ -18,8 +18,8 @@ mkDerivation rec {
   version = "2.31";
 
   src = fetchurl {
-    url = "https://github.com/electimon/metacity/releases/download/2.35.1/metacity-2.30.5.tar.gz";
-    sha256 = "sha256-ric06GdN4yodmy0E4C6c9hOC5eEyi5uXeSoYgmjJdhA";
+    url = "https://github.com/electimon/metacity/releases/download/2.35.1/metacity-v2.30.5.tar.gz";
+    sha256 = "sha256-XVi3Vva5eKzjZnNL6MST7RtLuabhwxpSEppBx5PYSX4=";
   };
 
   propagatedBuildInputs = [ which ]; # autogen.sh which is using gnome-common tends to require which
