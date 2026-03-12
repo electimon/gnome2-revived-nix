48e1361497929376a887af93dba119b5b6d510cb
Katou Megumi <electi@yzu.moe>
2026-03-12 17:43:37 +0000

    adding new metacity

--- platform/metacity/default.nix
+++ platform/metacity/default.nix
@@ -15,11 +15,11 @@
 
 mkDerivation rec {
   pname = "metacity";
-  version = "2.35";
+  version = "2.31";
 
   src = fetchurl {
-    url = "https://github.com/electimon/metacity/releases/download/2.35/metacity-2.34.21.tar.gz";
-    sha256 = "sha256-9dzSSvJkQOMcs0zmIWwmMzkF42tMmfEOlACbwzyOHHs=";
+    url = "https://github.com/electimon/metacity/releases/download/2.35.1/metacity-2.30.5.tar.gz";
+    sha256 = "sha256-ric06GdN4yodmy0E4C6c9hOC5eEyi5uXeSoYgmjJdhA";
   };
 
   propagatedBuildInputs = [ which ]; # autogen.sh which is using gnome-common tends to require which
